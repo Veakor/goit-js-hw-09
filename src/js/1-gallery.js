aad67a872c85263ddbe1d4f380a79ead78b49561
Veakor <135655247+Veakor@users.noreply.github.com>
2024-01-25 14:49:14 +0100

    add

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,5 @@
 
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 const images = [
